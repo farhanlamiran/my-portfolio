929f057ac81c1cfec6e8ed27165e637703dfc69b
Farhan Lamiran <empirevirtual21@gmail.com>
2025-08-04 15:13:06 +0700

    v2

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   devIndicators:false,
-  images: {
-    domains: ['images.unsplash.com'],
-  },
-};
+  };
 
 export default nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,21 @@ import ContactSection from '@/components/ContactSection';
 import SkillsSection from '@/components/SkillsSection';
 import ProjectsSection from '@/components/ProjectsSection';
 import HeroSection from '@/components/HeroSection';
+// pages/index.tsx atau pages/portfolio.tsx
+import Head from 'next/head'
 
 
 const Portfolio = () => {
 
   return (
+    <>
+      <Head>
+        <title>Farhan Lamiran | Web Developer</title>
+        <meta
+          name="description"
+          content="Portfolio website of Farhan Lamiran, a MERN stack developer."
+        />
+      </Head>
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900">
       <Navbar />
       <HeroSection />
@@ -17,6 +27,7 @@ const Portfolio = () => {
       <ContactSection />
       <Footer />
     </div>
+    </>
   );
 };
 
